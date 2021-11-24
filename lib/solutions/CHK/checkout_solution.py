9364a68d3bf3c8785a7d1163ab599070b34939bc
sbx_user1051 <sbx_user1051@169.254.8.181>
2021-11-24 16:48:17 +0000

    Wed Nov 24 16:48:17 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,24 +2,24 @@
 # skus = unicode string
 def checkout(skus):
 
-    item_prices = {
+    items = {
         "A": {"price": 50, "deals": [{"quantity": 5, "price": 200}, {"quantity": 3, "price": 130}]},
         "B": {"price": 30, "deals": [{"quantity": 2, "price": 45}]},
         "C": {"price": 20},
         "D": {"price": 15},
-        "E": {"price": 40, "deals": [{"quantity": 3, "price": 80}]}
+        "E": {"price": 40, "deals": [{"quantity": 3, "free": "B"}]}
     }
 
-    total_value = 0
+    total_cost = 0
     all_items = []
 
     for sku in skus:
-        if sku in item_prices:
+        if sku in items:
             all_items.append(sku)
         else:
             return -1
 
-    for item, item_details in item_prices.items():
+    for item, item_details in items.items():
 
         item_price = item_details["price"]
         item_deals = item_details.get("deals")
@@ -31,14 +31,19 @@ def checkout(skus):
 
             for deal in item_deals:
 
-                item_deal_quantity = deal["quantity"]
-                item_deal_price = deal["price"]
+                if deal.get("price"):
+                    item_deal_quantity = deal["quantity"]
+                    item_deal_price = deal["price"]
 
-                complete_deals = item_count//item_deal_quantity
-                total_value += (complete_deals * item_deal_price)
+                    complete_deals = item_count//item_deal_quantity
+                    total_cost += (complete_deals * item_deal_price)
 
-                item_count -= complete_deals * item_deal_quantity
+                    item_count -= complete_deals * item_deal_quantity
 
-        total_value += (item_count*item_price)
+                elif deal.get("free"):
+                    total_cost = items[deal["free"]]["price"]
 
-    return total_value
+        total_cost += (item_count*item_price)
+
+    return total_cost
+
